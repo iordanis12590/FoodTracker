7f0ffcef42600a4f602344a50eb436044014ba50
Basys <basys@eq76uxim.vpn.rrze.uni-erlangen.de>
2015-08-24 14:53:34 +0200

    reverting background color changes

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-//        self.view.backgroundColor = UIColor.blueColor()
         nameTextField.delegate = self
     }
 
